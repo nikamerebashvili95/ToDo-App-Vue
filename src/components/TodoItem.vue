03301abc476c3c9915a1345fe83bd1c38b66d9ad
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-10-17 22:37:37 +0400

    start project

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>TodoItem</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoItem",
+  props: {
+    name: String,
+    completed: Boolean,
+  },
+};
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>TodoList</h1>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem.vue";
+import axios from "axios";
+export default {
+  components: {
+    TodoItem,
+  },
+};
+</script>
