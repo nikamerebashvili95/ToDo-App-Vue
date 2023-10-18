77ee937db2cdebcc378593e6c218a76192dafd3f
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-10-18 13:56:11 +0400

    add axios and clearAll function

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -13,15 +13,9 @@
       <!-- task lists -->
       <div class="taskItems">
         <ul>
-          <li>
+          <li v-for="task in tasks" :key="task.id">
             <button class="toggle">
-              <i class="far fa-circle"></i>Learn Vue JS
-            </button>
-            <button><i class="far fa-trash-alt"></i></button>
-          </li>
-          <li>
-            <button class="toggle">
-              <i class="far fa-circle"></i>Watch netflix
+              <i class="far fa-circle"></i>{{ task.title }}
             </button>
             <button><i class="far fa-trash-alt"></i></button>
           </li>
@@ -30,7 +24,7 @@
       <!-- buttons -->
       <div class="clearBtns">
         <button>Clear completed</button>
-        <button>Clear all</button>
+        <button @click="clearAll">Clear all</button>
       </div>
       <!-- pending task -->
       <div class="pendingTasks">
@@ -42,10 +36,31 @@
 
 <script>
 import TodoItem from "./TodoItem.vue";
-
+import axios from "axios";
 export default {
   components: {
     TodoItem,
+  },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  methods: {
+    clearAll() {
+      this.tasks = [];
+    },
+  },
+  mounted() {
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos")
+      .then((response) => {
+        this.tasks = response.data;
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching todos:", error);
+      });
   },
 };
 </script>
