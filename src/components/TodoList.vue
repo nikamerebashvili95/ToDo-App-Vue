c600db00b331d21143b51b9d77f135007e77fa7c
Nika Merebashvili <114658102+nikamerebashvili95@users.noreply.github.com>
2023-10-18 13:36:16 +0400

    add list container and styling

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -10,6 +10,23 @@
         <input type="text" placeholder="New Task" />
         <button><i class="fas fa-plus"></i></button>
       </div>
+      <!-- task lists -->
+      <div class="taskItems">
+        <ul>
+          <li>
+            <button class="toggle">
+              <i class="far fa-circle"></i>Learn Vue JS
+            </button>
+            <button><i class="far fa-trash-alt"></i></button>
+          </li>
+          <li>
+            <button class="toggle">
+              <i class="far fa-circle"></i>Watch netflix
+            </button>
+            <button><i class="far fa-trash-alt"></i></button>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
